796fd1190cb5bf14b3b74ce1d416ddbcf7f6ceaf
Maher <109624708+mahernasser@users.noreply.github.com>
2024-03-23 23:08:19 +0200

    ..

---
+++ src/main/java/controllers/AdminAuthController.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+
+@WebServlet("admin/auth")
+public class AdminAuthController extends HttpServlet {
+
+}
